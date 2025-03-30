fb4ba5eb59ab08a7a936a4424040cbe21b889c1d
Tin Anišić <tin.anisic@hotmail.com>
2025-03-30 22:03:05 +0200

    chore: playing with winston in example app

--- apps/example-app/src/main.ts
+++ apps/example-app/src/main.ts
@@ -2,19 +2,48 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { RedocModule, RedocOptions } from 'nest-redoc';
-import * as bodyparser from 'body-parser';
 
+import { transports, format } from 'winston';
 import { extendZodWithOpenApi } from '@anatine/zod-openapi';
 import { z } from 'zod';
+import { WinstonModule } from 'nest-winston';
 extendZodWithOpenApi(z);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(
-    bodyparser.json({
-      type: 'application/vnd.api+json',
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        // let's log errors into its own file
+        new transports.File({
+          filename: `logs/error.log`,
+          level: 'error',
+          format: format.combine(format.timestamp(), format.json()),
+        }),
+        // logging all level
+        new transports.File({
+          filename: `logs/combined.log`,
+          format: format.combine(format.timestamp(), format.json()),
+        }),
+        // we also want to see logs in our console
+        new transports.Console({
+          format: format.combine(
+            format.cli(),
+            format.splat(),
+            format.timestamp(),
+            format.prettyPrint(),
+            format.printf((info) => {
+              return `${info.timestamp} ${info.level}: ${info.message}`;
+            }),
+          ),
+        }),
+      ],
     }),
-  );
+  });
+  // app.use(
+  //   bodyparser.json({
+  //     type: 'application/vnd.api+json',
+  //   }),
+  // );
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
